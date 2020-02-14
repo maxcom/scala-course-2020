774140bf62d2b91e4c61c9b4b0fc31d8a5701226
Maxim Valyanskiy <max.valjanski@gmail.com>
2020-02-14 16:36:21 +0300

    oops

--- code/LazyListDemo/src/main/scala/SimpleNumbers.scala
+++
@@ -1,8 +0,0 @@
-object SimpleNumbers extends App {
-
-  val numbers: LazyList[Int] = 2 +:
-    LazyList.from(3)
-    .filterNot(v => numbers.takeWhile(_ < v/2+1).exists(v % _ == 0))
-
-  numbers.take(10).foreach(println)
-}
